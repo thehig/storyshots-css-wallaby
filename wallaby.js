f436fdf197049fbb1f3d4c2790bfe2497ea2e0e6
David Higgins <thehig@gmail.com>
2018-01-20 16:51:50 +0000

    Add wallaby config

---
+++ wallaby.js
@@ -0,0 +1,53 @@
+module.exports = function(wallaby) {
+  // Babel, jest-cli and some other modules are located under
+  // react-scripts/node_modules, so need to let node.js know about it
+  var path = require("path");
+  process.env.NODE_PATH +=
+    path.delimiter +
+    path.join(__dirname, "node_modules") +
+    path.delimiter +
+    path.join(__dirname, "node_modules/react-scripts/node_modules");
+  require("module").Module._initPaths();
+
+  // Babel needs this
+  process.env.NODE_ENV = "development";
+
+  var babelCompiler = wallaby.compilers.babel({
+    babel: require("babel-core"),
+    presets: ["react-app"]
+  });
+
+  return {
+    files: [
+      "src/**/*.js",
+      "src/**/*.jsx",
+      "src/**/*.snap",
+      "!src/spec/**/*.spec.js",
+      "src/**/*.stories.js",
+      ".storybook/**/*.js"
+    ],
+    tests: [
+      "!src/spec/storyshots.spec.js", // FIXME: Storyshots don't play nice with wallaby at this time
+      "src/spec/**/*.spec.js"
+    ],
+    env: { type: "node", runner: "node" },
+    compilers: {
+      "**/*.js": babelCompiler,
+      ".storybook/**/*.js": babelCompiler
+    },
+    setup: wallaby => {
+      wallaby.testFramework.configure({
+        // as in node_modules/react-scripts/utils/createJestConfig.js
+        moduleNameMapper: {
+          "^.+\\.(jpg|jpeg|png|gif|svg)$": require.resolve(
+            "react-scripts/config/jest/fileTransform.js"
+          ),
+          "^.+\\.css$": require.resolve(
+            "react-scripts/config/jest/cssTransform.js"
+          )
+        }
+      });
+    },
+    testFramework: "jest"
+  };
+};
